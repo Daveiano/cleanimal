4605bdc7e72a7ade899154d8ebedc79e24b5fb84
dba <david.baetge@e-fork.de>
2020-01-03 00:22:32 +0100

    Added config/optional/block.block.cleanimal_*.yml files to set default blocks for regions. Removed js appending of views footer, added via template. Optimized scss files and templates. Removed teh experimental split screen mode and unused stuff from the .theme file.

--- js/cleanimal.js
+++ js/cleanimal.js
@@ -9,7 +9,7 @@
   Drupal.behaviors.cleanimal = {
     attach: function (context, settings) {
       // Add View Footer to Pager.
-      var $views = $('.view');
+     /* var $views = $('.view');
 
       $views.each(function () {
         var $viewFooter = $(this).find('.view-footer'),
@@ -18,7 +18,7 @@
         if ($pager.length && $viewFooter.length) {
           $pager.append($viewFooter);
         }
-      });
+      });*/
 
       // TODO: Show Views VBO Actions only if a row is selected.
     }
