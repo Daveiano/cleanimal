81aaa1c58e1fae68e78799771599a9d8986ea55c
dba <david.baetge@e-fork.de>
2020-01-03 13:56:17 +0100

    Updated package.json: removed react, babel and webpack, added simple node-sass scripts for generating css files from scss. Re-structured css file structure. Updated libraries for new structure. Reviewed theme file.

--- js/cleanimal.js
+++ js/cleanimal.js
@@ -8,18 +8,6 @@
 
   Drupal.behaviors.cleanimal = {
     attach: function (context, settings) {
-      // Add View Footer to Pager.
-     /* var $views = $('.view');
-
-      $views.each(function () {
-        var $viewFooter = $(this).find('.view-footer'),
-          $pager = $(this).find('.pager');
-
-        if ($pager.length && $viewFooter.length) {
-          $pager.append($viewFooter);
-        }
-      });*/
-
       // TODO: Show Views VBO Actions only if a row is selected.
     }
   };
