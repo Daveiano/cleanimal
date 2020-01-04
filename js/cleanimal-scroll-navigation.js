9aa474f05b0b6da6f6c7f5d44d19c4b3b8ee447e
dba <david.baetge@e-fork.de>
2020-01-04 15:22:29 +0100

    Added svg and styling for disabled checkboxes. Reviewed and optimized scroll navigation.

--- js/cleanimal-scroll-navigation.js
+++ js/cleanimal-scroll-navigation.js
@@ -6,8 +6,20 @@
 (function ($, Drupal) {
   'use strict';
 
+  var initialized;
+
+  function init() {
+    if (!initialized) {
+      initialized = true;
+
+      $('nav#scroll-nav ul li a').eq(0).addClass('active');
+    }
+  }
+
   Drupal.behaviors.cleanimal_scroll_navigation = {
     attach: function (context, settings) {
+
+      init();
 
       // Add class to tabs if scroll navigation is active so we can add a margin
       // show it at the same x-position like the main content.
@@ -15,113 +27,44 @@
         $(".region.region-pre-content nav.tabs").addClass("scroll-nav-active");
       }
 
-      // TODO: Review.
-      // Smooth scroll to section.
-      /*$("#scroll-nav a").click(function (e) {
+      // Smooth scroll to section on scroll-nav click.
+      $("#scroll-nav a").once().click(function (e) {
         e.preventDefault();
 
-        // TODO: Same as in lights.js.
-        var aid = $(this).attr("href"),
-          top = $(document).scrollTop(),
-          // Opened.
-          scrollTop = $(aid).offset().top - $('body').css('padding-top').replace('px', '') - 20,
-          $toolbarlining = $('.toolbar-tray'),
-          found = false;
+        var target = this.hash,
+          $target = $(target),
+          offsetTop = $target.offset().top;
 
-        $toolbarlining.each(function () {
-          if ($(this).hasClass('is-active')) {
-            found = true
-          }
+        // If fixed toolbar is active, update the offset and add 50px space.
+        if ($('body').hasClass('toolbar-tray-open') && $('body').hasClass('toolbar-horizontal') && $('body').hasClass('toolbar-fixed')) {
+          offsetTop = offsetTop - $('body').css('padding-top').replace('px', '') - 50;
+        }
+
+        $('html, body').stop().animate({
+          'scrollTop': offsetTop
+        }, 900, 'swing', function () {
+          window.location.hash = target;
         });
-
-        if (!found) {
-          scrollTop = $(aid).offset().top - $('body').css('padding-top').replace('px', '') - 59;
-        }
-
-        if (top === 0) {
-          // Opened Submenu.
-          if (found) {
-            scrollTop = $(aid).offset().top - $('body').css('padding-top').replace('px', '') - 80;
-          }
-          // Closed.
-          else {
-            scrollTop = $(aid).offset().top - $('body').css('padding-top').replace('px', '') - 119;
-          }
-        }
-
-        if ($(this).parent().index() === 0) {
-          scrollTop = 0;
-        }
-
-        $('html,body').animate({scrollTop: scrollTop},'fast');
       });
 
       // Set nav link to active.
       $(window).scroll(function () {
-        var
-          windscroll = $(window).scrollTop(),
-          $nodeSecondary = $('.layout-region.layout-region-node-secondary'),
-          $body = $('body'),
-          $header = $('header.content-header'),
-          $secondaryNav = $('.block.block-core.block-local-tasks-block'),
-          bodyPadding = parseInt($body.css('padding-top')),
-          // @todo dynamic header height is not working because of animating header to specific height have to do it static
-          //headerHeight = $header.outerHeight(),
-          headerHeight = 120,
-          $toolbarlining = $('.toolbar-tray'),
-          found = false;
+        var scrollPos = $(window).scrollTop(),
+          fixedTopHeight = 0;
 
-        $toolbarlining.each(function () {
-          if ($(this).hasClass('is-active')) {
-            found = true
-          }
-        });
-
-        if ($body.hasClass('node-add-edit')) {
-          if (windscroll > 0) {
-
-            $header.addClass('fixed small');
-            $header.css("top", bodyPadding + "px");
-
-            $secondaryNav.addClass('fixed');
-            $secondaryNav.css("top", headerHeight + bodyPadding + "px");
-
-            $('.page-content').addClass('padding-top');
-
-          }
-          else {
-            $header.removeClass('fixed small');
-            $secondaryNav.removeClass('fixed');
-            $('.page-content').removeClass('padding-top');
-          }
+        if ($('body').hasClass('toolbar-tray-open') && $('body').hasClass('toolbar-horizontal') && $('body').hasClass('toolbar-fixed')) {
+          fixedTopHeight = $('body').css('padding-top').replace('px', '') - 50;
         }
 
-        if (windscroll > 0) {
-          $('nav#scroll-nav').addClass('sticky');
-          $nodeSecondary.addClass('sticky');
-        }
-        else {
-          $('nav#scroll-nav').removeClass('sticky');
-          $nodeSecondary.removeClass('sticky');
-        }
+        $('.scroll-nav-content').each(function (i) {
+          var scrollSectionOffset = $(this).offset().top;
 
-        $('.field-group-html-element.scroll-nav-content').each(function (i) {
-          if (found) {
-            if (($(this).position().top - $('body').css('padding-top').replace('px', '') - 30) <= windscroll) {
-              $('nav#scroll-nav a.active').removeClass('active');
-              $('nav#scroll-nav a').eq(i).addClass('active');
-            }
+          if ((scrollSectionOffset - scrollPos - fixedTopHeight) <= 0) {
+            $('nav#scroll-nav ul li a').removeClass('active');
+            $('nav#scroll-nav ul li a').eq(i).addClass('active');
           }
-          else {
-            if (($(this).position().top - $('body').css('padding-top').replace('px', '') - 69) <= windscroll) {
-              $('nav#scroll-nav a.active').removeClass('active');
-              $('nav#scroll-nav a').eq(i).addClass('active');
-            }
-          }
-        });
-
-      }).scroll();*/
-
+        })
+      });
     }
   }
 })(jQuery, Drupal);
