9a79cf458f63d5f1d126c5ee5f49161facd7fcb3
dba <david.baetge@e-fork.de>
2020-01-13 19:49:59 +0100

    Added js to move messages on node-edit-add pages to the main content region. Optimized chosen component css. Fixed some php warnings and removed some unnecessary todos and comments.

--- js/cleanimal.js
+++ js/cleanimal.js
@@ -9,6 +9,11 @@
   Drupal.behaviors.cleanimal = {
     attach: function (context, settings) {
       // TODO: Show Views VBO Actions only if a row is selected.
+
+      // Move the messages on the node-add-edit page to the content region.
+      if ($("body").hasClass("node-add-edit") && $(".status-messages-wrap").length) {
+        $(".status-messages-wrap").prependTo(".layout-region.layout-region-node-main");
+      }
     }
   };
 
